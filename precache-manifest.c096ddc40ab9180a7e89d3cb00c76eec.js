fed8f9eddbcdf78447f3739c7827b13e5de4e797
rawah <rawah@pdx.edu>
2020-06-02 19:32:07 -0700

    Updates

---
+++ precache-manifest.c096ddc40ab9180a7e89d3cb00c76eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e909711aac3084ae23431db7bbe5c9",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "e5cd6e80375f3836cb47",
+    "url": "/dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78fe5325d96b298fed95",
+    "url": "/dashboard/static/js/2.bd57b5c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard/static/js/2.bd57b5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5cd6e80375f3836cb47",
+    "url": "/dashboard/static/js/main.cf9bc5f2.chunk.js"
+  },
+  {
+    "revision": "6ce332862785f968006a",
+    "url": "/dashboard/static/js/runtime-main.8fb46908.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
