593504faff0b82d8575cce03334c30e79e37efbf
rawah <rawah@pdx.edu>
2020-06-04 19:05:51 -0700

    adding the table of the countries and cases in each one to the DataTable component

--- src/components/Table/DataTable.js
+++ src/components/Table/DataTable.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
-
+import { TableContainer, Table,TableHead, TableRow, TableCell, Paper, TableBody} from '@material-ui/core';
+import { MDBDataTable } from 'mdbreact';
 
 const DataTable  = ({stats}) => {
   if(!stats) {
     return 'loading'
   }
-
-  return(
+/*
     <div> Table:
     <Table striped bordered hover variant="dark">
       <thead>
@@ -28,6 +27,77 @@ const DataTable  = ({stats}) => {
       </tbody>
       </Table>
       </div>
+  */
+
+
+  // this one worked better
+  /*<TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableCell> Country </TableCell>
+          <TableCell> Confirmed</TableCell>
+        </TableHead>
+
+        <TableBody>
+          { (stats.length > 0)? 
+            stats.map((item, i) => (
+              <TableRow key= {item.country}>
+                <TableCell component="th" scope="row">
+                  {item.country}
+                </TableCell>
+                <TableCell>{item.cases}</TableCell>
+            </TableRow>)) : null}
+
+          
+        </TableBody>
+      </Table>
+            </TableContainer>*/
+
+  /*const row = stats.map(item => <>{item.country}</>) 
+  console.log(row)
+  const data = {
+    colomns: [
+      {
+        label:'Country',
+        field: 'country',
+        width: 150
+      },{
+        label:'Confirmed cases',
+        field: 'confirmed',
+        width:150
+      }
+    ],
+    rows: [
+    ]
+  }*/
+  return(     
+    <div>
+      <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableCell> Country </TableCell>
+          <TableCell> Confirmed</TableCell>
+        </TableHead>
+
+        <TableBody>
+          { (stats.length > 0)? 
+            stats.map((item, i) => (
+              <TableRow key= {item.country}>
+                <TableCell component="th" scope="row">
+                  {item.country}
+                </TableCell>
+                <TableCell>{item.cases}</TableCell>
+            </TableRow>)) : null}
+
+          
+        </TableBody>
+      </Table>
+      </TableContainer>
+    </div> 
+    
+
+
+
   )
 }
 export default DataTable;
