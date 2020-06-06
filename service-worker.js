be99153d253999f7ade4cd816afbbe74f02dcfc8
rawah <rawah@pdx.edu>
2020-06-05 23:19:13 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.61e94d0d5af538088d6385a81950fab1.js"
+  "/dashboard/precache-manifest.f5de3486e001f9b5e773fa5176616d39.js"
 );
 
 self.addEventListener('message', (event) => {
